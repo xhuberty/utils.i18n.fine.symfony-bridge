c8c34458cddd10a8747a6cc3247e2009a7a78473
David Négrier <d.negrier@thecodingmachine.com>
2014-04-23 09:52:22 +0200

    initial import

---
+++ src/Mouf/Utils/I18n/Fine/Bridge/SymfonyFineBridge.php
@@ -0,0 +1,95 @@
+<?php
+namespace Mouf\Utils\I18n\Fine\Bridge;
+
+use Symfony\Component\Translation\TranslatorInterface;
+use Mouf\Utils\I18n\Fine\Language\FixedLanguageDetection;
+use Mouf\Utils\I18n\Fine\Language\CascadingLanguageDetection;
+use Mouf\Utils\I18n\Fine\Translate\LanguageTranslationInterface;
+use Mouf\Utils\I18n\Fine\Language\LanguageDetectionInterface;
+
+/**
+ * This class is a bridge between a FINE translation service and Symfony.
+ * Using this bridge, you can use FINE in a Symfony component.
+ * 
+ * Note: there is not a 1..1 mapping between Fine and Symfony.
+ * Fine expects parameters to be passed as a series of parameters in the function while
+ * Symfony takes an associative array.
+ * 
+ * - the transChoice is simulated by concatenating the id and the number
+ * So $service->transChoice("key", 2) will be translated in Fine in $service->getMessage("key2") 
+ */
+class SymfonyFineBridge implements TranslatorInterface {
+	
+	/**
+	 * @var LanguageTranslationInterface
+	 */
+	private $translator;
+	
+	/**
+	 * 
+	 * @var LanguageDetectionInterface
+	 */
+	private $detector;
+	
+	/**
+	 * Used if the "setLocale" method is called.
+	 * 
+	 * @var FixedLanguageDetection
+	 */
+	private $fixedLanguageDetection;
+	
+	/**
+	 * Used to cascade FixedLanguageDetection with the provide language detection.
+	 *
+	 * @var CascadingLanguageDetection
+	 */
+	private $cascadingLanguageDetection;
+	
+	public function __construct(LanguageTranslationInterface $translator, LanguageDetectionInterface $detector) {
+		$this->translator = $translator;
+		$this->detector = $detector;
+		$this->fixedLanguageDetection = new FixedLanguageDetection();
+		$this->cascadingLanguageDetection = new CascadingLanguageDetection();
+		$this->cascadingLanguageDetection->languageDetectionServices = [
+			$this->fixedLanguageDetection,
+			$this->detector
+		];
+	}
+	
+	
+	/* (non-PHPdoc)
+	 * @see \Symfony\Component\Translation\TranslatorInterface::trans()
+	 */
+	public function trans($id, array $parameters = array(), $domain = null, $locale = null) {
+		// Note: $domain is ignored.
+		// Note: $locale is ignored.
+		array_unshift($parameters, $id);
+		if ($this->translator->hasTranslation($id)) {
+			return call_user_func_array([$this->translator, 'getTranslation'], $parameters);
+		} else {
+			return false;
+		}
+		
+	}
+
+	/* (non-PHPdoc)
+	 * @see \Symfony\Component\Translation\TranslatorInterface::transChoice()
+	 */
+	public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null) {
+		return $this->trans($id.$number, $parameters, $domain, $locale);
+	}
+
+	/* (non-PHPdoc)
+	 * @see \Symfony\Component\Translation\TranslatorInterface::setLocale()
+	 */
+	public function setLocale($locale) {
+		$this->fixedLanguageDetection->setLanguage($locale);
+	}
+
+	/* (non-PHPdoc)
+	 * @see \Symfony\Component\Translation\TranslatorInterface::getLocale()
+	 */
+	public function getLocale() {
+		return $this->cascadingLanguageDetection->getLanguage();
+	}
+}
