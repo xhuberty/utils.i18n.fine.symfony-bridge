8f5e70aba8923243541ae321c92efdf5695b5666
David Négrier <d.negrier@thecodingmachine.com>
2014-04-23 10:15:31 +0200

    Adding installer

---
+++ src/Mouf/Utils/I18n/Fine/Bridge/SymfonyFineBridgeInstaller.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2013 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+namespace Mouf\Utils\Common\Doctrine\Annotations;
+
+use Mouf\Installer\PackageInstallerInterface;
+use Mouf\MoufManager;
+use Mouf\Actions\InstallUtils;
+
+/**
+ * The installer for the Symfony-Fine bridge
+ */
+class SymfonyFineBridgeInstaller implements PackageInstallerInterface {
+
+	/**
+	 * (non-PHPdoc)
+	 * @see \Mouf\Installer\PackageInstallerInterface::install()
+	 */
+	public static function install(MoufManager $moufManager) {
+	
+		// These instances are expected to exist when the installer is run.
+		$defaultTranslationService = $moufManager->getInstanceDescriptor('defaultTranslationService');
+		$defaultLanguageDetection = $moufManager->getInstanceDescriptor('defaultLanguageDetection');
+		
+		// Let's create the instances.
+		$symfonyTranslator = InstallUtils::getOrCreateInstance('symfonyTranslator', 'Mouf\\Utils\\I18n\\Fine\\Bridge\\SymfonyFineBridge', $moufManager);
+		
+		// Let's bind instances together.
+		if (!$symfonyTranslator->getConstructorArgumentProperty('translator')->isValueSet()) {
+			$symfonyTranslator->getConstructorArgumentProperty('translator')->setValue($defaultTranslationService);
+		}
+		if (!$symfonyTranslator->getConstructorArgumentProperty('detector')->isValueSet()) {
+			$symfonyTranslator->getConstructorArgumentProperty('detector')->setValue($defaultLanguageDetection);
+		}
+
+		// Let's rewrite the MoufComponents.php file to save the component
+		$moufManager->rewriteMouf();
+	}
+}
